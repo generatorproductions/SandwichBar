373f575cd7281d2f85b0e929f8d06f6346526a0b
Hunter <hunterufford@gmail.com>
2019-03-01 11:55:17 -0800

    Initial sharing of project

---
+++ SandwitchMaker.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class SandwitchMaker here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SandwitchMaker
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SandwitchMaker
+     */
+    public SandwitchMaker()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}

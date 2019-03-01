81b40457402fec696fe5163b8eb2ca69c0c2cb35
Hunter <hunterufford@gmail.com>
2019-03-01 12:24:46 -0800

    Sandwich list

--- SandwichList.java
+++ SandwichList.java
@@ -4,7 +4,23 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.*;
+
 public class SandwichList
 {
-    
+    ArrayList<Sandwich> menu = new ArrayList<Sandwich>();
+    public void add(Sandwich s)
+    {
+        if(menu.size() <= 3)
+        {
+            menu.add(s);
+            System.out.println("Sanwich " + s + " added to the menu in slot " + menu.size());
+        }
+        else
+        {
+            System.out.println("Menu is full!");
+        }
+        
+        
+    }
 }

4377a7b5f74655a0ab43420e22251815e3361635
Angelo Bravo <frogs65xiwin@gmail.com>
2019-03-01 12:33:34 -0800

    df

--- CustomerList.java
+++ CustomerList.java
@@ -10,6 +10,14 @@ public class CustomerList
     private ArrayList<Customer> types = new ArrayList<Customer>();
     public void add(Customer c)
     {
-        
+        if(types.size() <= 10)
+        {
+            types.add(c);
+            System.out.println("Customer " + c + " is in line");
+        }
+        else
+        {
+            System.out.println("Line is full!");
+        }
     }
 }

c321bd5f01eb0359712d0fd3c81fdb3022d547bc
Angelo Bravo <frogs65xiwin@gmail.com>
2019-03-01 12:26:55 -0800

    interface

--- CustomerList.java
+++ CustomerList.java
@@ -4,7 +4,12 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.ArrayList;
 public class CustomerList
 {
-    
+    private ArrayList<Customer> types = new ArrayList<Customer>();
+    public void add(Customer c)
+    {
+        
+    }
 }

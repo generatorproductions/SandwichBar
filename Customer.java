1f6064278ff9c222dc2b962d49897a3fa9ee7aed
Hunter <33736724+Babuntska@users.noreply.github.com>
2019-03-04 11:23:02 -0800

    Update Customer.java
    Added more names

--- Customer.java
+++ Customer.java
@@ -6,9 +6,9 @@
  */
 public interface Customer
 {
-    String[][] names = {{"Cole", "Gage", "Mila", "Kevin", "Sonny"},/* Easy */
-                        {"Gibbs", "Angelo", "Hunter", "Jordan"},/* Mid */
-                        {"Klugg", "Faix", "Jablinski", "Joyce"}};/* Difficult */
+    String[][] names = {{"Cole", "Gage", "Myla", "Kevin", "Sonny","Granny"},/* Easy */
+                        {"Gibbs", "Angelo", "Hunter", "Jordan","Mrs.Puff", "Guy Fieri's Wife"},/* Mid */
+                        {"Mr. Klug", "Mr. Faix", "Jablinski", "Mr. Joyce", "Bubble Bass", "Gene Scallop","Mrs. Obama", "Madame Hagfish"}};/* Difficult */
     public String response();
     public int rate();
     public Sandwich order();

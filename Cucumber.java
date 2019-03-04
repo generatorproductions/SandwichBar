b4a16564a3880c885f4346dbe64e014a66c8e885
Angelo Bravo <frogs65xiwin@gmail.com>
2019-03-04 12:40:41 -0800

    Added Vegetables

---
+++ Cucumber.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Cucumber here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+abstract public class Cucumber extends Veggie
+{
+    
+}

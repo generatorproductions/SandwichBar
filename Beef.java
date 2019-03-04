711790df054f5a4504cb5701382bb8d40790bc33
Jordan <rudman21918074@hotmail.com>
2019-03-04 12:41:40 -0800

    Added proteins

---
+++ Beef.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class Beef here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+abstract public class Beef extends Protein
+{
+    
+}
---
+++ Turkey.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class Meat here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+abstract public class Turkey extends Protein
+{
+    
+}

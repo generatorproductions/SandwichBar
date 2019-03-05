12fcc3c3485722a5bb16a01863a20b157df2ea62
Angelo Bravo <frogs65xiwin@gmail.com>
2019-03-05 12:40:42 -0800

    Bring back Kale

---
+++ Pepper.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Pepper here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Pepper extends Veggie
+{
+    public String noise()
+    {
+        return "Pop";
+    }
+}

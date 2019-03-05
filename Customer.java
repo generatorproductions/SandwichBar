c78df3eca356005098aed0fdcc6312e3a176fb2f
Jordan <rudman21918074@hotmail.com>
2019-03-05 12:34:41 -0800

    Added ZywOo, fixed spellings

--- Customer.java
+++ Customer.java
@@ -7,8 +7,8 @@
 public interface Customer
 {
     String[][] names = {{"Cole", "Gage", "Myla", "Kevin", "Sonny","Granny", "Officer Parish", "Gilbert 'Ratio' Reyes", "Paul Blart"},/* Easy */
-                        {"Mr. Gibbs", "Angelo", "Hunter", "Jordan", "Mrs. Puff", "Guy Fieri", "Perch Perkins", "MatPat", "MC Steve", "Dan Gheesling"},/* Mid */
-                        {"Mr. Klug", "Mr. Faix", "Jablinski", "Mr. Joyce", "Bubble Bass", "Gene Scallop", "Stu Cabe", "Mrs. Obama", "Tori Christiansen"}};/* Difficult */
+                        {"Mr. Gibbs", "Angelo", "Hunter", "Jordan", "Mrs. Puff", "Guy Fieri", "Perch Perkins", "MatPat", "MC Steve", "Dan Gheesling", "ZywOo"},/* Mid */
+                        {"Mr. Klug", "Mr. Faix", "Jablinski", "Mr. Joyce", "Bubble Bass", "Gene Scallop", "Stu Cabe", "Mrs. Obama", "Tory Christensen"}};/* Difficult */
     public String response();
     public int rate();
     public Sandwich order();

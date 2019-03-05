797d58bcec3c4436aa84385fd47f387b65a3575b
Hunter <hunterufford@gmail.com>
2019-03-05 12:44:06 -0800

    Yee-Haw

---
+++ Hummus.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Hummus here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Hummus extends Sauce
+{
+    public String noise()
+    {
+        return "Yee-Haw";
+    }
+}
---
+++ Tofu.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Tofu here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Tofu extends Protein
+{
+    public String noise()
+    {
+        return "Clap";
+    }
+}

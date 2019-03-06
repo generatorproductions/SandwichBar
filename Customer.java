4e940e9a18414f21cf3c94228ad7390c5705067e
Angelo Bravo <frogs65xiwin@gmail.com>
2019-03-06 13:20:13 -0800

    Responses

--- Customer.java
+++ Customer.java
@@ -9,6 +9,9 @@ public interface Customer
     String[][] names = {{"Cole", "Gage", "Myla", "Kevin", "Sonny","Granny", "Officer Parish", "Gilbert 'Ratio' Reyes", "Paul Blart"},/* Easy */
                         {"Mr. Gibbs", "Angelo", "Hunter", "Jordan", "Mrs. Puff", "Guy Fieri", "Perch Perkins", "MatPat", "MC Steve", "Dan Gheesling", "ZywOo"},/* Mid */
                         {"Mr. Klug", "Mr. Faix", "Jablinski", "Mr. Joyce", "Bubble Bass", "Gene Scallop", "Stu Cabe", "Mrs. Obama", "Tory Christensen"}};/* Difficult */
+    String [][] goodResponses = {{"...", "Mayonnaise", "Yeet", "", "Yum", "Thank ya kiddo", "I wanna get back to work", "Nice calculation", "Thanks bud"},
+                                 {"Constitutional", "I don't eat sandwiches", "*Whips*", "Thanks bro", "Get back to class", "I've arrived at Flavortown", "Wicked", "Epic", "", "Let's go", "Hehe, I don't know"},
+                                 {"", "That's pretty good", "Oh yeah", "Thanks", "I suppose this sandwich is adequate", ""}}; 
     public String response();
     public String getName();
 }

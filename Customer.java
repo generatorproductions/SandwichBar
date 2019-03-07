3fb79da8046ddd8be4f24ee9c61450126b254724
Angelo Bravo <frogs65xiwin@gmail.com>
2019-03-07 12:53:39 -0800

    dydx

--- Customer.java
+++ Customer.java
@@ -10,8 +10,11 @@ public interface Customer
                         {"Mr. Gibbs", "Angelo", "Hunter", "Jordan", "Mrs. Puff", "Guy Fieri", "Perch Perkins", "MatPat", "Dan Gheesling", "ZywOo"}, /* Mid */
                         {"Mr. Klug", "Mr. Faix", "Jablinski", "Mr. Joyce", "Bubble Bass", "Gene Scallop", "Stu Cabe", "Mrs. Obama", "Gordon Ramsay", "Gerald"}}; /* Difficult */
     String [][] goodResponses = {{"*Smiles*", "Mayonnaise.", "Yeet.", "Mmmmm.", "Yum.", "Thank ya, kiddo!", "Great, thanks!", "Thanks, bud.", "Glad I left my stoop for this!", "*High five*"},/* Easy */
-                                 {"Fantastic.", "I don't eat sandwiches, but okay.", "Good stuff.", "Thanks, bro.", "Scrumptious!", "I've arrived at Flavortown.", "Wicked!", "Epic!", "Let's go!", "Hehe, I don't know (Good)."}, /* Mid */
-                                 {"[INSERT RESPONSE]", "That's pretty good.", "Oh yeah!", "Thanks.", "I suppose this sandwich is adequate.", "Phenomenal.", "Alright!", "That's one mighty fine sandwich!", "I guess this is alright for a dump like this.", "Far out!"}}; /* Difficult */
+                                 {"Constitutional.", "I don't eat sandwiches, but okay.", "I like that.", "That's it, chief.", "Scrumptious!", "I've arrived at Flavortown.", "Wicked!", "Epic!", "Let's go!", "Hehe, I don't know (Good)."}, /* Mid */
+                                 {"That is fantabulous", "That's pretty good.", "Oh yeah!", "Thanks.", "I suppose this sandwich is adequate.", "Phenomenal.", "Alright!", "That's one mighty fine sandwich!", "I guess this is alright for a dump like this.", "Far out!"}}; //difficult
+    String [][] badResponses = {{"...", "Mayonnaise", "Reeee", "[INSERT RESPONSE]", "Eww", "Do better next time kiddo", "Good try", "", "I shouldn't have left my stoop for this", "*Unenthusiastic high-five*"}, 
+                                {"[INSERT RESPONSE]", "I don't like sandwiches anyway", "Nah that ain't for me", "That ain't it chief", "", "No dawg", ""},
+                                {"The person who made this is nucking futs", "[INSERT RESPONSE]", "", "[INSERT RESPONSE]", "You forgot the pickles", "*Walks out the door*", ""}};
     public String response();
     public String getName();
 }

497fd71f58977e012210ede40d0f4bfc9f336af2
Hunter <hunterufford@gmail.com>
2019-03-07 13:25:30 -0800

    afsdagdgdag

--- Menu.java
+++ Menu.java
@@ -14,12 +14,14 @@ public class Menu
     private String[] hardOptions = {"Sb", "Sr", "Sh", "Pg", "Ev", "Lt", "Cb", "On","Pe", "Be", "Tk", "Ci","Tf", "Md", "Ma", "Hs","Mm", "Rb"};
     private String[] parts = {"RegularBun","SeededBun","SlicedGrain","SlicedWhite","PlainBagel","EverythingBagel","Lettuce","Cucumber","Onion","Pepper","Beef","Turkey", "Chicken","Tofu", "Mustard", "Mayo", "Hot Sause","Hummus"};
     private String menuString = "";
+    private String difficulty = "";
     public Menu(String diff)
     {
         
         int i = 0;
         if(diff.equals("e") || diff.equalsIgnoreCase("easy"))
         {
+            difficulty = "easy";
             menuString += "\n";
             for(String s : easyOptions)
             {
@@ -34,6 +36,7 @@ public class Menu
         }
         else if(diff.equals("h") || diff.equalsIgnoreCase("hard"))
         {
+            difficulty = "hard";
             menuString += "\n";
             for(String s : hardOptions)
             {
@@ -48,9 +51,24 @@ public class Menu
         }
     }
     
+    public String[][] getValues()
+    {
+        String[][] result = new String[2][parts.length];
+        if(difficulty.equals("easy"))
+        {
+            result[0] = easyOptions;
+            result[1] = parts;
+        }
+        if(difficulty.equals("hard"))
+        {
+            result[0] = hardOptions;
+            result[1] = parts;
+        }
+        return result;
+    }
+    
     public void printMenu()
     {
-        
         System.out.print(menuString);
     }
     
---
+++ Reader.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Reader here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+public class Reader
+{
+    
+    private Scanner s = new Scanner(System.in);
+    public Sandwich readLine()
+    {
+        String line = s.nextLine();
+        
+        
+        
+        
+        
+    }
+}

1dcfc06b0e66f72e63d3d69545cc7456aa7db070
Jordan <rudman21918074@hotmail.com>
2019-03-06 21:10:31 -0800

    Edited characters and responses

--- Customer.java
+++ Customer.java
@@ -6,12 +6,12 @@
  */
 public interface Customer
 {
-    String[][] names = {{"Cole", "Gage", "Myla", "Kevin", "Sonny","Granny", "Officer Parish", "Gilbert 'Ratio' Reyes", "Paul Blart"},/* Easy */
-                        {"Mr. Gibbs", "Angelo", "Hunter", "Jordan", "Mrs. Puff", "Guy Fieri", "Perch Perkins", "MatPat", "MC Steve", "Dan Gheesling", "ZywOo"},/* Mid */
-                        {"Mr. Klug", "Mr. Faix", "Jablinski", "Mr. Joyce", "Bubble Bass", "Gene Scallop", "Stu Cabe", "Mrs. Obama", "Tory Christensen"}};/* Difficult */
-    String [][] goodResponses = {{"*Smiles*", "Mayonnaise", "Yeet", "Mmmmm", "Yum", "Thank ya kiddo", "I wanna get back to work", "Nice calculation", "Thanks bud"},
-                                 {"Constitutional", "I don't eat sandwiches", "*Whips*", "Thanks bro", "Get back to class", "I've arrived at Flavortown", "Wicked", "Epic", "", "Let's go", "Hehe, I don't know"},
-                                 {"", "That's pretty good", "Oh yeah", "Thanks", "I suppose this sandwich is adequate", "Phenomenal", "That's a big elephant sandwich", "That's one mighty fine sandwich", "*Gives a high-five*"}}; 
+    String[][] names = {{"Cole", "Gage", "Myla", "Kevin", "Sonny", "Granny", "Arnold", "Paul Blart", "Stoop Kid", "Tory Christensen"}, /* Easy */
+                        {"Mr. Gibbs", "Angelo", "Hunter", "Jordan", "Mrs. Puff", "Guy Fieri", "Perch Perkins", "MatPat", "Dan Gheesling", "ZywOo"}, /* Mid */
+                        {"Mr. Klug", "Mr. Faix", "Jablinski", "Mr. Joyce", "Bubble Bass", "Gene Scallop", "Stu Cabe", "Mrs. Obama", "Gordon Ramsay", "Gerald"}}; /* Difficult */
+    String [][] goodResponses = {{"*Smiles*", "Mayonnaise.", "Yeet.", "Mmmmm.", "Yum.", "Thank ya, kiddo!", "Great, thanks!", "Thanks, bud.", "Glad I left my stoop for this!", "*High five*"},/* Easy */
+                                 {"Fantastic.", "I don't eat sandwiches, but okay.", "Good stuff.", "Thanks, bro.", "Scrumptious!", "I've arrived at Flavortown.", "Wicked!", "Epic!", "Let's go!", "Hehe, I don't know (Good)."}, /* Mid */
+                                 {"[INSERT RESPONSE]", "That's pretty good.", "Oh yeah!", "Thanks.", "I suppose this sandwich is adequate.", "Phenomenal.", "Alright!", "That's one mighty fine sandwich!", "I guess this is alright for a dump like this.", "Far out!"}}; /* Difficult */
     public String response();
     public String getName();
 }

a5d485a84c4cef81022f0ca308ad44c9eae46629
Jordan <rudman21918074@hotmail.com>
2019-03-11 12:45:07 -0700

    Attempted fix for spaces, didn't work

--- SandwichMaker.java
+++ SandwichMaker.java
@@ -58,8 +58,8 @@ public class SandwichMaker
         Customer midC = new MidCustomer();
         Customer diffC = new DifficultCustomer();
         System.out.println(easyC.getName() + ": I'd like a " + createEasySandy() + ".");
-        System.out.println(midC.getName() + ": I'd like a " +createMidSandy() + ".");
-        System.out.println(diffC.getName() + ": I'd like a " +createDiffSandy() + ".");
+        System.out.println(midC.getName() + ": I'd like a " + createMidSandy() + ".");
+        System.out.println(diffC.getName() + ": I'd like a " + createDiffSandy() + ".");
         /*
         Sandwich sand1 = new Sandwich(g, p, s, v);
         Sandwich sand2 = new Sandwich(g, p, s, l);

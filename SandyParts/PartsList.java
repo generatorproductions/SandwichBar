25d574dd9327baa933098a5a581ff2b17a1bc524
Hunter <33736724+Babuntska@users.noreply.github.com>
2019-03-15 08:24:01 -0700

    Update PartsList.java

--- SandyParts/PartsList.java
+++ SandyParts/PartsList.java
@@ -2,10 +2,10 @@ package SandyParts;
 
 
 /**
- * Write a description of class SandyPartsList here.
+ * A list for all SandwichParts and abbreviations
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Generator Productions
+ * @version 1.0
  */
 import java.util.*;
 
@@ -15,31 +15,52 @@ public class PartsList
     private static ArrayList<String> easyAbbrev = new ArrayList<String>();
     private static ArrayList<String> hardAbbrev = new ArrayList<String>();
     
+    /**
+     * @param : A SandwichParts
+     * Adds a SandwichParts to an ArrayList
+     */
     public void add(SandwichParts g)
     {
         parts.add(g);
     }
     
+    /**
+    * @param : A String of an abbreviation
+    * Addes the easy abbreviation to the easyAbbrev ArrayList
+    */
     public void addEasyAbrev(String s)
     {
         easyAbbrev.add(s);
     }
     
+    /**
+    * @param : A String of an abbreviation
+    * Addes the hard abbreviation to the hardAbbrev ArrayList
+    */
     public void addHardAbrev(String s)
     {
         hardAbbrev.add(s);
     }
     
+    /**
+    * @return: returns an ArrayList of sandwich parts.
+    */
     public ArrayList<SandwichParts> getParts()
     {   
         return parts;
     }
     
+    /**
+    *   @return: returns an ArrayList of abbreveations
+    */
     public ArrayList<String> getEasyAbbrev()
     {   
         return easyAbbrev;
     }
     
+    /**
+    *   @return: returns an ArrayList of abbreveations
+    */
     public ArrayList<String> getHardAbbrev()
     {   
         return hardAbbrev;
